ca9eeba730f797123c0da2c5259478509bb43b13
Oliver Ruoff <oliver93ruoff@web.de>
2020-04-14 18:45:01 +0200

    added pid to kalman

--- bot/robot.py
+++ bot/robot.py
@@ -120,14 +120,38 @@ compAngleY = pitch;
 
 timer = time.time()
 flag = 0
-y_init_values = []
-y_angle_offset = 0
+
+setpoint = -92
+min_motor_speed = 55 # required for motors to start turning
+
+Kp = 30
+Ki = 0
+Kd = 0
+
+pid = PID(Kp, Ki, Kd, setpoint=setpoint, sample_time=0.007, output_limits=(-100, 100))
+v = 0
+
+old_time = time.time()
+
 while True:
     if(flag >100): #Problem with the connection
         print("There is a problem with the connection")
         flag=0
         continue
     try:
+
+        control = int(pid(v))
+        if control < 0:
+            pt.move_front()
+        else:
+            pt.move_back()
+        control = abs(control)
+        control = min_motor_speed if control < min_motor_speed else control
+        pt.change_speed_all(control)
+        current_time = time.time()
+        print('V:', v, '| control:', control, '| Time elapsed (s):', current_time-old_time, 'PID wights:', pid.components)
+        old_time = current_time
+        
         #Read Accelerometer raw value
         accX = read_raw_data(ACCEL_XOUT_H)
         accY = read_raw_data(ACCEL_YOUT_H)
@@ -191,12 +215,10 @@ while True:
         if ((gyroYAngle < -180) or (gyroYAngle > 180)):
             gyroYAngle = kalAngleY
 
-        if len(y_init_values) < 100:
-            y_init_values.append(kalAngleY)
-        else:
-            y_angle_offset = sum(y_init_values)/len(y_init_values)
-        if y_angle_offset != 0:
-            print("Angle Y: " + str(kalAngleY-y_angle_offset))
+        print("Angle Y: " + str(kalAngleY))
+        v = kalAngleY
+        
+
         # print(str(roll)+"  "+str(gyroXAngle)+"  "+str(compAngleX)+"  "+str(kalAngleX)+"  "+str(pitch)+"  "+str(gyroYAngle)+"  "+str(compAngleY)+"  "+str(kalAngleY))
         # time.sleep(0.005)
 
