6a0a06cd0bbcbe78c21bc54a8cd2bd48381dd6bc
Oliver Ruoff <oliver93ruoff@web.de>
2020-04-24 22:17:17 +0200

    removed sleep time

--- bot/robot.py
+++ bot/robot.py
@@ -34,11 +34,10 @@ mpu = mpu6050.mpu6050()
 
 pt.change_speed_all(0)
 
-def emergency_stop_check(angle, min_angle=-20, max_angle=23, sleep_time=0.2):
+def emergency_stop_check(angle, min_angle=-20, max_angle=23):
     if angle > max_angle or angle < min_angle:
         print('Angle: %f. Seems like I fell. Waiting for help.'%(angle))
         pt.break_motors()
-        time.sleep(sleep_time)
         angle = mpu.get_angle()[0]
         emergency_stop_check(angle)
 
