ecfd58eaa9768f730837581e2c81b53c4784f5fa
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2021-04-23 00:26:55 +0200

    removed is stuck functionality for now

--- bot/robot.py
+++ bot/robot.py
@@ -37,16 +37,16 @@ GPIO.add_event_detect(STABILITY_SWITCH_PIN, GPIO.BOTH,
                       callback=stability_switch_changed, bouncetime=200)
 
 
-def is_stuck(angle, min_angle=-20, max_angle=20):
-    if angle > max_angle or angle < min_angle:
-        print('Angle: %f. Seems like I fell. Waiting for help.' % (angle))
-        speed = pt.get_speed_all()
-        if speed[0] > 0 or speed[1] > 0:
-            pt.change_speed_all(0)
-            pt.break_motors()
-        return True
-    else:
-        return False
+# def is_stuck(angle, min_angle=-20, max_angle=20):
+#    if angle > max_angle or angle < min_angle:
+#        print('Angle: %f. Seems like I fell. Waiting for help.' % (angle))
+#        speed = pt.get_speed_all()
+#        if speed[0] > 0 or speed[1] > 0:
+#            pt.change_speed_all(0)
+#            pt.break_motors()
+#        return True
+#    else:
+#        return False
 
 # Testing
 # while True:
@@ -65,8 +65,8 @@ try:
     while(True):
         angle_info = mpu.get_angle()
         v = angle_info[0]
-        if is_stuck(v):
-            continue
+        # if is_stuck(v):
+        #    continue
         control = int(pid(v))
         if v > setpoint:
             stepper.turn_stepper(1)
