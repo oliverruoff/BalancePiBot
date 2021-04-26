8c5db43807181b9c1c4d2780f06785ae3d77d604
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2021-04-26 22:45:07 +0200

    Added drive object

--- bot/robot.py
+++ bot/robot.py
@@ -4,7 +4,7 @@ import math
 import RPi.GPIO as GPIO
 from simple_pid import PID
 
-from movement import stepper
+from movement import drive
 from sensing import mpu6050
 
 # IMPORTANT VARIABLES TO CONFIGURE -------------------
@@ -40,29 +40,41 @@ def stability_switch_changed(channel):
     print(GPIO.input(STABILITY_SWITCH_PIN))
 
 
-# Wait for the input to go low, run the function when it does
-GPIO.add_event_detect(STABILITY_SWITCH_PIN, GPIO.BOTH,
-                      callback=stability_switch_changed, bouncetime=200)
+if __name__ == '__main__':
+    # Wait for the input to go low, run the function when it does
+    GPIO.add_event_detect(STABILITY_SWITCH_PIN, GPIO.BOTH,
+                          callback=stability_switch_changed, bouncetime=200)
 
+    drive = drive(12,
+                  25,
+                  21,
+                  20,
+                  4,
+                  steps_per_revolution=200,
+                  delay=.0005)
 
-pid = PID(Kp, Ki, Kd, setpoint=setpoint,
-          sample_time=0.008, output_limits=(-100, 100))
-old_time = time.time()
+    drive.activate_stepper()
 
-try:
-    while(True):
-        angle_info = mpu.get_angle()
-        v = angle_info[0] - ANGLE_OFFSET
-        control = int(pid(v))
-        if v > setpoint:
-            stepper.turn_stepper(10)
-        else:
-            stepper.turn_stepper(10, False)
-        control = abs(control)
-        control = min_motor_speed if control < min_motor_speed else control
-        # pt.change_speed_all(control)
-        print('V:', v, '| control:', control, '| Frequency:',
-              angle_info[3], '| PID weights:', pid.components)
+    drive.turn_stepper(self, 1000, clockwise=True)
 
-except KeyboardInterrupt:
-    print('Stopped!')
+    pid = PID(Kp, Ki, Kd, setpoint=setpoint,
+              sample_time=0.008, output_limits=(-100, 100))
+    old_time = time.time()
+
+    try:
+        while(True):
+            angle_info = mpu.get_angle()
+            v = angle_info[0] - ANGLE_OFFSET
+            control = int(pid(v))
+            if v > setpoint:
+                stepper.turn_stepper(10)
+            else:
+                stepper.turn_stepper(10, False)
+            control = abs(control)
+            control = min_motor_speed if control < min_motor_speed else control
+            # pt.change_speed_all(control)
+            print('V:', v, '| control:', control, '| Frequency:',
+                  angle_info[3], '| PID weights:', pid.components)
+
+        except KeyboardInterrupt:
+            print('Stopped!')
