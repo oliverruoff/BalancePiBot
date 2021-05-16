36fcecb555e5f77128c6c876f109a6ae1c5afe71
Ruoff Oliver (XC-AD/ECS2) <oliver.ruoff@de.bosch.com>
2021-05-16 14:51:57 +0200

    breaking: Update to dc motors

--- _deprecated/robot.py
+++ _deprecated/robot.py
@@ -1,26 +1,34 @@
 import time
+import math
 
 import RPi.GPIO as GPIO
 from simple_pid import PID
 
-from actuators import l298n
-from sensors import mpu6050
+from movement import drive
+from sensing import mpu6050
 
 # IMPORTANT VARIABLES TO CONFIGURE -------------------
 
-setpoint = 0
+setpoint = 7
 
 Kp = 1
 Ki = 0
 Kd = 0
 
+ANGLE_OFFSET = 0
+
 # IMPORTANT VARIABLES TO CONFIGURE -------------------
-GPIO_MODE = GPIO.BCM
-GPIO.setmode(GPIO_MODE)
+
+GPIO.setmode(GPIO.BCM)
 
 # STABILITY SWITCH
 STABILITY_SWITCH_PIN = 16
 GPIO.setup(STABILITY_SWITCH_PIN, GPIO.IN)
+
+# Stepper activation pin
+STEPPER_ACTIVATOR_PIN = 24
+GPIO.setup(STEPPER_ACTIVATOR_PIN, GPIO.OUT)
+GPIO.output(STEPPER_ACTIVATOR_PIN, GPIO.input(STABILITY_SWITCH_PIN))
 
 mpu = mpu6050.mpu6050()
 
@@ -35,48 +43,51 @@ if __name__ == '__main__':
     # GPIO.add_event_detect(STABILITY_SWITCH_PIN, GPIO.BOTH,
     #                     callback=stability_switch_changed, bouncetime=200)
 
-    motor_driver = l298n.l298n(
-        in1_pin=19,
-        in2_pin=13,
-        in3_pin=6,
-        in4_pin=5,
-        ena_pin=26,
-        enb_pin=11,
-        gpio_mode=GPIO_MODE)
+    drive = drive.drive(
+        left_direction_pin=12,
+        left_step_pin=25,
+        right_direction_pin=21,
+        right_step_pin=20,
+        activator_pin=STEPPER_ACTIVATOR_PIN,
+        steps_per_revolution=200)
 
-    try:
-        motor_driver.change_left_duty_cycle(20)
-        motor_driver.change_left_duty_cycle(20)
-        time.sleep(10000)
-    except KeyboardInterrupt:
-        motor_driver.stop_motors()
-        print('Stopped!')
-
-    time.sleep(20)
+    drive.activate_stepper()
 
     pid = PID(Kp, Ki, Kd, setpoint=setpoint, output_limits=(0, 100))
     old_time = time.time()
+
+    # init & and start steppers
+    drive.turn_both_steppers()
 
     # cycle used for activation switch checks
     cycle = 0
     try:
         while(True):
             angle_info = mpu.get_angle()
-            v = angle_info[0]
+            v = angle_info[0] - ANGLE_OFFSET
             control = int(pid(v))
             control = abs(control)
             print('V:', v, '| control:', control, '| Frequency:',
                   angle_info[3], '| PID weights:', pid.components)
 
+            # switch activated code
+            if cycle % 100 == 0:
+                cycle = 0
+                if GPIO.input(STABILITY_SWITCH_PIN) == 0:
+                    drive.deactivate_stepper()
+                    time.sleep(0.2)
+                    continue
+                drive.activate_stepper()
+            cycle += 1
+            # switch activated code
+
             if v > setpoint:
-                motor_driver.change_left_motor_direction(True)
-                motor_driver.change_right_motor_direction(True)
+                drive.set_stepper_rotation_clockwise(True)
             else:
-                motor_driver.change_left_motor_direction(False)
-                motor_driver.change_right_motor_direction(False)
+                drive.set_stepper_rotation_clockwise(False)
 
             # drive.change_speed_all(control)
 
     except KeyboardInterrupt:
-        motor_driver.stop_motors()
+        drive.deactivate_stepper()
         print('Stopped!')
---
+++ bot/actuators/l298n.py
@@ -0,0 +1,94 @@
+# Motor driver control for driver l298n
+# author: Oliver Ruoff
+
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+
+class l298n:
+
+    def __init__(
+        self,
+        in1_pin,
+        in2_pin,
+        in3_pin,
+        in4_pin,
+        ena_pin,
+        enb_pin,
+        gpio_mode=GPIO.BCM
+    ):
+
+        self.in1_pin = in1_pin
+        self.in2_pin = in2_pin
+        self.in3_pin = in3_pin
+        self.in4_pin = in4_pin
+        self.left_duty_cycle = 0
+        self.right_duty_cycle = 0
+        self.left_motor_direction = True
+        self.right_motor_direction = True
+
+        # initializing pins
+        GPIO.setmode(gpio_mode)
+        GPIO.setup(in1_pin, GPIO.OUT)
+        GPIO.setup(in2_pin, GPIO.OUT)
+        GPIO.setup(in3_pin, GPIO.OUT)
+        GPIO.setup(in4_pin, GPIO.OUT)
+        GPIO.setup(ena_pin, GPIO.OUT)
+        GPIO.setup(enb_pin, GPIO.OUT)
+
+        # setting all pins to low at start
+        GPIO.output(in1_pin, GPIO.LOW)
+        GPIO.output(in2_pin, GPIO.LOW)
+        GPIO.output(in3_pin, GPIO.LOW)
+        GPIO.output(in4_pin, GPIO.LOW)
+
+        # right motor
+        p_a = GPIO.PWM(ena_pin, 1000)
+        self.p_a = p_a
+        p_a.start(0)
+
+        # left motor
+        p_b = GPIO.PWM(enb_pin, 1000)
+        self.p_b = p_b
+        p_b.start(0)
+
+        p_a.ChangeDutyCycle(self.left_duty_cycle)
+        p_b.ChangeDutyCycle(self.right_duty_cycle)
+
+    def change_left_duty_cycle(self, duty_cycle):
+        self.left_duty_cycle = duty_cycle
+        self.p_a.ChangeDutyCycle(duty_cycle)
+
+    def change_right_duty_cycle(self, duty_cycle):
+        self.right_duty_cycle = duty_cycle
+        self.p_b.ChangeDutyCycle(duty_cycle)
+
+    def change_left_motor_direction(self, clockwise):
+        if clockwise:
+            GPIO.output(self.in1_pin, GPIO.HIGH)
+            GPIO.output(self.in2_pin, GPIO.LOW)
+        else:
+            GPIO.output(self.in1_pin, GPIO.LOW)
+            GPIO.output(self.in2_pin, GPIO.HIGH)
+
+    def change_right_motor_direction(self, clockwise):
+        if clockwise:
+            GPIO.output(self.in3_pin, GPIO.HIGH)
+            GPIO.output(self.in4_pin, GPIO.LOW)
+        else:
+            GPIO.output(self.in3_pin, GPIO.LOW)
+            GPIO.output(self.in4_pin, GPIO.HIGH)
+
+    def stop_left_motor(self):
+        GPIO.output(self.in1_pin, GPIO.LOW)
+        GPIO.output(self.in2_pin, GPIO.LOW)
+
+    def stop_right_motor(self):
+        GPIO.output(self.in3_pin, GPIO.LOW)
+        GPIO.output(self.in4_pin, GPIO.LOW)
+
+    def stop_motors(self):
+        self.stop_left_motor()
+        self.stop_right_motor()
